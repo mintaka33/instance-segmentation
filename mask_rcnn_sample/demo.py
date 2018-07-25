833643509497a7356191ed515b27003fe5e960ee
mintaka33 <mintaka33@outlook.com>
2018-07-25 01:51:51 -0400

    add source code

---
+++ mask_rcnn_sample/demo.py
@@ -0,0 +1,129 @@
+#from __future__ import print_function,division
+import os
+import sys
+sys.path.append('./coco3/PythonAPI')
+import random
+import math
+import numpy as np
+import scipy.misc
+import matplotlib
+#import matplotlib.pyplot as plt
+
+import coco
+import utils
+import model as modellib
+import visualize_cap as vc
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Path to trained weights file
+# Download this file and place in the root of your 
+# project (See README file for details)
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+# Load a random image from the images folder
+# Load a random image from the images folder
+import cv2
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = scipy.misc.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+#cap = cv2.VideoCapture(0)
+imgindex = 0
+#plt.ion()
+
+while(True):
+    #ret, image = cap.read()
+    image = cv2.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+    # Run detection
+    results = model.detect([image], verbose=1)
+    print (image.shape)
+    # Visualize results
+    r = results[0]
+    boxes = r['rois']
+    masks = r['masks'] 
+    class_ids = r['class_ids'] 
+    class_names = class_names
+    scores=r['scores']
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+    height, width = image.shape[:2]
+
+    # Generate random colors
+    colors = vc.random_colors(N)
+
+    masked_image = image.astype(np.uint8).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        cv2.imwrite('Mask-Rcnn.jpg',masked_image)
+        cv2.rectangle(masked_image, (x1,y1),(x2,y2), (0,255,0),2)
+        # Label
+        class_id = class_ids[i]
+        score = scores[i] if scores is not None else None
+        label = class_names[class_id]
+        x = random.randint(x1, (x1 + x2) // 2)
+        caption = "{} {:.3f}".format(label, score) if score else label
+        # Mask
+        mask = masks[:, :, i]
+        masked_image = vc.apply_mask(masked_image, mask, color)
+
+        cv2.putText(masked_image, caption, (int(x1),int(y1)),cv2.FONT_HERSHEY_SIMPLEX,1, (255,0,255),2)
+
+    cv2.imshow('Mask-Rcnn',masked_image)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27 or key == ord('q'):
+        break
+    elif key == ord('s'):
+        imgindex += 1
+        cv2.imwrite('./{0}.jpg'.format(imgindex),masked_image)
+cap.release()
+cv2.destroyAllWindows()
+
